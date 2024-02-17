a4d2022f6914fae0bde5d27006f63847d7300aff
Julius Bächle <juliusbaechle@users.noreply.github.com>
2024-02-17 09:59:23 +0100

    Update Servo Component Test

--- Software/Component_Tests/Servo/Servo.ino
+++ Software/Component_Tests/Servo/Servo.ino
@@ -1,24 +1,33 @@
 #include <Servo.h>
-Servo servo;
 
-// 8 VL
-// 7 RR
-// 5 VR
-// 4 RL
+Servo servos[4];
 
 void setup() {
   Serial.begin(115200);
-  servo.attach(7);
-  servo.writeMicroseconds(1000);
+  servos[0].attach(8); // FL / ESC4
+  servos[1].attach(5); // FR / ESC2
+  servos[2].attach(4); // RL / ESC3
+  servos[3].attach(7); // RR / ESC1
+  for(int i = 0; i < 4; i++)
+    servos[i].writeMicroseconds(1000);
 }
 
 void loop() {
   if(Serial.available()) {
     String str = Serial.readString();
     str.trim();
-    long us = str.toInt();
-    servo.writeMicroseconds(us);
-    Serial.print("Wrote: ");
-    Serial.println(us);
+    
+    long percent = str.toInt();
+    if (percent > 40)
+      percent = 40;
+    if (percent < 0)
+      percent = 0;
+    
+    for(int i = 0; i < 4; i++)
+      servos[i].writeMicroseconds(1000 + 10 * percent);
+      
+    Serial.print("wrote ");
+    Serial.print(percent);
+    Serial.println(" %");
   }
 }
